df208e247ff16a6d4a48e471404bc0c844b7eeff
Nimrat Khalsa <nimratkhalsa16@icloud.com>
2024-09-10 21:12:13 +0530

    Initial Commit

---
+++ LocalNotifs/LocalNotifsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalNotifsApp.swift
+//  LocalNotifs
+//
+//  Created by Nimrat Khalsa on 9/10/24.
+//
+
+import SwiftUI
+
+@main
+struct LocalNotifsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
